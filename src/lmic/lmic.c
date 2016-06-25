0d69adf0fccee42e7bf635f1af79600a83c5c596
Oliv4945 <oliv4945@gmail.com>
2016-06-25 23:26:39 +0200

    Parse MAC command LinkCheckAns

--- src/lmic/lmic.c
+++ src/lmic/lmic.c
@@ -1093,8 +1093,8 @@ static bit_t decodeFrame (void) {
     while( oidx < olen ) {
         switch( opts[oidx] ) {
         case MCMD_LCHK_ANS: {
-            //int gwmargin = opts[oidx+1];
-            //int ngws = opts[oidx+2];
+            LMIC.gwMargin = opts[oidx+1];
+            LMIC.nGws = opts[oidx+2];
             oidx += 3;
             LMIC.lchkReq = 0;       // Request only once
             continue;
@@ -2146,6 +2146,8 @@ void LMIC_reset (void) {
     LMIC.dn2Dr        =  DR_DNW2;   // we need this for 2nd DN window of join accept
     LMIC.dn2Freq      =  FREQ_DNW2; // ditto
     LMIC.lchkReq      = 0;
+    LMIC.gwMargin     = 0;
+    LMIC.nGws         = 0;
 #if !defined(DISABLE_PING)
     LMIC.ping.freq    =  FREQ_PING; // defaults for ping
     LMIC.ping.dr      =  DR_PING;   // ditto
